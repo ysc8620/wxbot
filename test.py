f9ed2fb66c0603535a65b221e99ea87df2f7e876
ysc8620 <ysc8620@163.com>
2016-04-06 17:25:56 +0800

    ds

---
+++ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from wxbot import *
+
+
+class MyWXBot(WXBot):
+    def handle_msg_all(self, msg):
+        if msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
+            self.send_msg_by_uid('hi', msg['user']['id'])
+
+
+'''
+    def schedule(self):
+        self.send_msg('tb', 'schedule')
+        time.sleep(1)
+'''
+
+
+def main():
+    bot = MyWXBot()
+    bot.DEBUG = True
+    bot.conf['qr'] = 'png'
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
